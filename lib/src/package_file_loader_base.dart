243599adb7ca8a15fd1c414b524ad4d0b7318edf
Oliver Atienza <oliatienza@gmail.com>
2022-11-02 10:13:52 +0800

    Prepare for initial release

--- lib/src/package_file_loader_base.dart
+++ lib/src/package_file_loader_base.dart
@@ -1,20 +1,73 @@
 import 'dart:io';
 
-/// packages:test/load_this_file.dart
-Future<File> loadPackageFile(String path) async {
-  final packageRegex = RegExp(r'^package:(\w+)\/(.*)');
+import 'package:build/build.dart';
+import 'package:collection/collection.dart';
+
+final packageRegex = RegExp(r'^package:(\w+)\/(.*)$');
+
+class _LoadedFile {
+  _LoadedFile({
+    required this.file,
+    required this.packageName,
+    required this.path,
+  });
+
+  final File file;
+  final String packageName;
+  final String path;
+}
+
+class LoadedFileAsset {
+  LoadedFileAsset({
+    required this.assetId,
+    required this.file,
+  });
+
+  final AssetId assetId;
+  final File file;
+}
+
+Future<File> loadPackageFile(String path) async => (await _loadPackageFile(path)).file;
+
+Future<LoadedFileAsset> loadPackageFileAsAsset(String path) async {
+  final info = await _loadPackageFile(path);
+  return LoadedFileAsset(assetId: AssetId(info.packageName, info.path), file: info.file);
+}
+
+Future<_LoadedFile> _loadPackageFile(String path) async {
   final match = packageRegex.firstMatch(path);
   if (match == null) {
-    throw Error();
+    throw FormatException(
+        "Invalid format for package reference. Expected format r'^package:(\\w+)/(.*)\$'");
   }
+
   final packageName = match.group(1);
+  if (packageName == null) {
+    throw FormatException('Could not extract package name from path');
+  }
   final filePath = match.group(2);
+  if (filePath == null) {
+    throw FormatException('Could not extract file location from path');
+  }
 
   final p = '$packageName:';
   final file = File('${Directory.current.path}/.packages');
+  if (!file.existsSync()) {
+    throw FileSystemException('Packages index not found. Run flutter pub get first');
+  }
   final lines = await file.readAsLines();
-  final line = lines.firstWhere((e) => e.startsWith(p));
+  final line = lines.firstWhereOrNull((e) => e.startsWith(p));
+  if (line == null) {
+    throw PackageNotFoundException(
+        '$packageName, make sure that the dependency is added to pubspec and run flutter pub get');
+  }
   final packagePath = line.replaceFirst(p, '').replaceFirst('file://', '');
 
-  return File.fromUri(Uri(scheme: 'file', path: '$packagePath$filePath'));
+  return _LoadedFile(
+    packageName: packageName,
+    path: filePath,
+    file: File.fromUri(
+      Uri(scheme: 'file', path: '$packagePath$filePath'),
+    ),
+  );
 }
